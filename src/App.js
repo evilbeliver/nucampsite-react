8e1d1b253853eb0d432e0fb30e4fa73c11ec6c00
evilbeliver <82883551+evilbeliver@users.noreply.github.com>
2023-08-06 15:25:19 -0400

    Week 1 Task 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Container, Navbar, NavbarBrand } from 'reactstrap';
 import NucampLogo from './app/assets/img/logo.png';
-import CampsiteCard from './features/campsites/CampsiteCard.js';
-import { CAMPSITES } from './app/shared/CAMPSITES';
+import CampsitesList from './features/campsites/CampsitesList';
 import './App.css';
+import { CAMPSITES } from './app/shared/CAMPSITES';
 
 function App() {
     return (
@@ -15,7 +15,7 @@ function App() {
                     </NavbarBrand>
                 </Container>
             </Navbar>
-            <CampsiteCard campsite={CAMPSITES[0]} />
+            <CampsitesList campsite={CAMPSITES[0]} />
         </div>
     );
 }
