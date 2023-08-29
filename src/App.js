579ae84e2e48f8ea2d7e8ce8f0e8d8a76db8d802
evilbeliver <82883551+evilbeliver@users.noreply.github.com>
2023-08-29 15:42:26 -0400

    Async Partners

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import AboutPage from './pages/AboutPage';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
+import { fetchPartners } from './features/parters/partnersSlice';
 import './App.css';
 
 
@@ -17,6 +18,7 @@ function App() {
     const dispatch = useDispatch();
     useEffect(() => {
         dispatch(fetchCampsites());
+        dispatch(fetchPartners());
     }, [dispatch]);
 
     return (
--- src/features/parters/PartnersList.js
+++ src/features/parters/PartnersList.js
@@ -1,22 +1,32 @@
 import { useSelector } from 'react-redux';
-import { Col } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import Partner from '../../components/Partner';
 import { selectAllPartners } from './partnersSlice';
+import Error from '../../components/Error';
+import Loading from '../../components/Loading';
 
 const PartnersList = () => {
     const partners = useSelector(selectAllPartners);
+    const isLoading = useSelector((state) => state.partners.isLoading);
+    const errMsg = useSelector((state) => state.partners.errMsg);
 
-return (
-    <Col className='mt-4'>
-        {partners.map((partner) => {
-            return (
-                <div className='d-flex mb-5' key={partner.id}>
-                    <Partner partner={partner} />
-                </div>
-            );
-        })}
-    </Col>
-);
+    return isLoading ? (
+        <Loading />
+    ) : errMsg ? (
+        <Error errMsg={errMsg} />
+    ) : (
+        <Col className='mt-4'>
+            <Row>
+                {partners.map((partner) => {
+                    return (
+                        <div className='d-flex mb-5' key={partner.id}>
+                            <Partner partner={partner} />
+                        </div>
+                    );
+                })}
+            </Row>
+        </Col>
+    );
 };
 
 export default PartnersList;
